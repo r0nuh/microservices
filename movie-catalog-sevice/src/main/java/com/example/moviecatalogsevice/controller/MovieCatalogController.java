c1b814a43accab9712835c2ba0f4071c036295c0
r0nuh <cs.szilard@gmail.com>
2019-08-20 23:08:54 +0200

    (Movie Catalog Service): use RestTemplate with dependency injection

--- movie-catalog-sevice/src/main/java/com/example/moviecatalogsevice/controller/MovieCatalogController.java
+++ movie-catalog-sevice/src/main/java/com/example/moviecatalogsevice/controller/MovieCatalogController.java
@@ -1,23 +1,38 @@
 package com.example.moviecatalogsevice.controller;
 
 import com.example.moviecatalogsevice.models.CatalogItem;
+import com.example.moviecatalogsevice.models.Movie;
+import com.example.moviecatalogsevice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping(value = "/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(
-                new CatalogItem("The Matrix", "test", 5)
+        List<Rating> ratings = Arrays.asList(
+                new Rating(1, 5),
+                new Rating(1234, 3),
+                new Rating(1235, 4)
         );
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getTitle(), "test", rating.getRating());
+        })
+                .collect(Collectors.toList());
     }
 }
