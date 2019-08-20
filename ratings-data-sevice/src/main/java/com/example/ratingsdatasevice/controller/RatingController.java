1984f75983928981fa1c8afb934653450a7dafd7
r0nuh <cs.szilard@gmail.com>
2019-08-21 00:06:11 +0200

    (Ratings Data Service): create and use UserRating model having a list of ratings property

--- ratings-data-sevice/src/main/java/com/example/ratingsdatasevice/controller/RatingController.java
+++ ratings-data-sevice/src/main/java/com/example/ratingsdatasevice/controller/RatingController.java
@@ -1,17 +1,34 @@
 package com.example.ratingsdatasevice.controller;
 
 import com.example.ratingsdatasevice.models.Rating;
+import com.example.ratingsdatasevice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingController {
 
     @GetMapping("/{movieId}")
-    public Rating getRating(@PathVariable int movieId){
+    public Rating getRatingByMovie(@PathVariable int movieId){
         return new Rating(movieId, 5);
+    }
+
+    @GetMapping("users/{userId}")
+    public UserRating getRatingByUser(@PathVariable String userId){
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating(1, 5),
+                new Rating(1234, 3),
+                new Rating(1235, 4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setRatings(ratings);
+        return userRating;
     }
 }
