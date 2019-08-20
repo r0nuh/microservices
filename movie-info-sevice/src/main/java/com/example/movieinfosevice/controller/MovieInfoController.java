4ca5aca76a633ba79316682370de02af8af9f3d4
r0nuh <cs.szilard@gmail.com>
2019-08-20 22:20:59 +0200

    (Movie Info Service): take movieId from path

--- movie-info-sevice/src/main/java/com/example/movieinfosevice/controller/MovieInfoController.java
+++ movie-info-sevice/src/main/java/com/example/movieinfosevice/controller/MovieInfoController.java
@@ -12,7 +12,7 @@ public class MovieInfoController {
 
     @GetMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable int movieId){
-        return new Movie(1, "The Matrix");
+        return new Movie(movieId, "The Matrix");
     }
 
 }
