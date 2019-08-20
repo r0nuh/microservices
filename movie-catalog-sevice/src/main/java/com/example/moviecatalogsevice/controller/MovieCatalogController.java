48136ce60bb811390d3155b0b4eff72ad302c1e2
r0nuh <cs.szilard@gmail.com>
2019-08-20 23:26:34 +0200

    (Movie Catalog Service): use WebClient with dependency injection

--- movie-catalog-sevice/src/main/java/com/example/moviecatalogsevice/controller/MovieCatalogController.java
+++ movie-catalog-sevice/src/main/java/com/example/moviecatalogsevice/controller/MovieCatalogController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +20,7 @@ import java.util.stream.Collectors;
 public class MovieCatalogController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping(value = "/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
@@ -30,7 +31,12 @@ public class MovieCatalogController {
                 new Rating(1235, 4)
         );
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
             return new CatalogItem(movie.getTitle(), "test", rating.getRating());
         })
                 .collect(Collectors.toList());
