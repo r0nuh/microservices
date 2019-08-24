40b431a10c101c08e9f41e18e44f6c34b4ab0650
r0nuh <cs.szilard@gmail.com>
2019-08-24 22:52:37 +0200

    (Movie Catalog Service): use service names in the restTemplate instead of hardcoded urls

--- movie-catalog-sevice/src/main/java/com/example/moviecatalogsevice/controller/MovieCatalogController.java
+++ movie-catalog-sevice/src/main/java/com/example/moviecatalogsevice/controller/MovieCatalogController.java
@@ -23,11 +23,11 @@ public class MovieCatalogController {
     @GetMapping(value = "/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
                 //ParameterizedTypeReference<List<Rating>>{});
 
         return ratings.getRatings().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getTitle(), "test", rating.getRating());
         }).collect(Collectors.toList());
     }
